eb35b1100100a47490873f550bdaaeb7b22ff3f4
Andreas Möller <am@localheinz.com>
2020-03-09 23:21:06 +0100

    Fix: Move method

--- test/Doctrine/Fixtures/TestCase.php
+++ test/Doctrine/Fixtures/TestCase.php
@@ -2,6 +2,9 @@
 
 namespace FactoryGirl\Tests\Provider\Doctrine\Fixtures;
 
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\Tools\SchemaTool;
 use PHPUnit\Framework;
 use FactoryGirl\Tests\Provider\Doctrine\TestDb;
 use FactoryGirl\Provider\Doctrine\FixtureFactory;
@@ -19,6 +22,39 @@ abstract class TestCase extends Framework\TestCase
     {
         parent::setUp();
 
-        $this->em = TestDb::createEntityManager();
+        $this->em = self::createEntityManager();
+    }
+
+    final protected static function createEntityManager(): EntityManager
+    {
+        $annotationPath = __DIR__ . '/../../Fixture/Entity';
+        $proxyDir = __DIR__ . '/../Fixtures/TestProxy';
+        $proxyNamespace = 'FactoryGirl\Tests\Provider\Doctrine\Fixtures\TestProxy';
+
+        $cache = new ArrayCache();
+
+        $config = new Configuration();
+        $config->setMetadataCacheImpl($cache);
+        $config->setQueryCacheImpl($cache);
+        $config->setMetadataDriverImpl(
+            $config->newDefaultAnnotationDriver($annotationPath)
+        );
+        $config->setProxyDir($proxyDir);
+        $config->setProxyNamespace($proxyNamespace);
+        $config->setAutoGenerateProxyClasses(true);
+
+        $em = EntityManager::create(
+            [
+                'driver' => 'pdo_sqlite',
+                'path' => ':memory:'
+            ],
+            $config
+        );
+
+        $tool = new SchemaTool($em);
+
+        $tool->createSchema($em->getMetadataFactory()->getAllMetadata());
+
+        return $em;
     }
 }
--- test/Doctrine/TestDb.php
+++ test/Doctrine/TestDb.php
@@ -16,39 +16,4 @@ use Doctrine\ORM\Configuration;
  */
 class TestDb
 {
-    /**
-     * @return EntityManager
-     */
-    public static function createEntityManager()
-    {
-        $annotationPath = __DIR__ . '/../Fixture/Entity';
-        $proxyDir = __DIR__ . '/Fixtures/TestProxy';
-        $proxyNamespace = 'FactoryGirl\Tests\Provider\Doctrine\Fixtures\TestProxy';
-
-        $cache = new ArrayCache();
-
-        $config = new Configuration();
-        $config->setMetadataCacheImpl($cache);
-        $config->setQueryCacheImpl($cache);
-        $config->setMetadataDriverImpl(
-            $config->newDefaultAnnotationDriver($annotationPath)
-        );
-        $config->setProxyDir($proxyDir);
-        $config->setProxyNamespace($proxyNamespace);
-        $config->setAutoGenerateProxyClasses(true);
-
-        $em = EntityManager::create(
-            [
-                'driver' => 'pdo_sqlite',
-                'path'   => ':memory:'
-            ],
-            $config
-        );
-
-        $tool = new SchemaTool($em);
-
-        $tool->createSchema($em->getMetadataFactory()->getAllMetadata());
-
-        return $em;
-    }
 }
