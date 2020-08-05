b994063709433813476f382ff82a2d8106d26268
Andreas Möller <am@localheinz.com>
2020-08-05 17:01:01 +0200

    Fix: Add WithoutOptionalStrategy

---
+++ src/Strategy/WithoutOptionalStrategy.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2020 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/factory-bot
+ */
+
+namespace Ergebnis\FactoryBot\Strategy;
+
+use Ergebnis\FactoryBot\Count;
+use Ergebnis\FactoryBot\FieldDefinition;
+use Ergebnis\FactoryBot\FixtureFactory;
+use Faker\Generator;
+
+/**
+ * @internal
+ */
+final class WithoutOptionalStrategy implements ResolutionStrategy
+{
+    public function resolveFieldValue(
+        Generator $faker,
+        FixtureFactory $fixtureFactory,
+        FieldDefinition\Resolvable $fieldDefinition
+    ) {
+        if ($fieldDefinition instanceof FieldDefinition\Optional) {
+            return null;
+        }
+
+        return $fieldDefinition->resolve(
+            $faker,
+            $fixtureFactory
+        );
+    }
+
+    public function resolveCount(Generator $faker, Count $count): int
+    {
+        return $count->minimum();
+    }
+}
